885b898479b906db44b42cb092af22bafbd4e9e5
Garima <garimachhikara128@gmail.com>
2021-02-03 22:09:37 +0530

    Linked List

--- src/L31_LL/LinkedList.java
+++ src/L31_LL/LinkedList.java
@@ -5,6 +5,15 @@ public class LinkedList {
 	private class Node {
 		int data;
 		Node next;
+
+		public Node() {
+
+		}
+
+		public Node(int data) {
+			this.data = data;
+		}
+
 	}
 
 	private Node head;
@@ -262,4 +271,155 @@ public class LinkedList {
 
 	}
 
+	public void reverseI() {
+
+		Node prev = null;
+		Node curr = head;
+
+		while (curr != null) {
+
+			Node ahead = curr.next;
+			curr.next = prev;
+
+			prev = curr;
+			curr = ahead;
+		}
+
+		head = prev;
+
+	}
+
+	public void reverseR() {
+		// reverseR(null, head);
+
+		Node temp = head;
+
+		reverseR(head);
+
+		temp.next = null;
+	}
+
+	private void reverseR(Node prev, Node curr) {
+
+		if (curr == null) {
+			head = prev;
+			return;
+		}
+
+//		Node ahead = curr.next ;
+//		
+//		curr.next = prev;
+
+//		reverseR(curr, ahead);
+
+		reverseR(curr, curr.next);
+
+		curr.next = prev;
+
+	}
+
+	private void reverseR(Node curr) {
+
+		if (curr.next == null) {
+			head = curr;
+			return;
+		}
+
+		reverseR(curr.next);
+
+		curr.next.next = curr;
+
+	}
+
+	public int mid() {
+
+		Node slow = head;
+		Node fast = head;
+
+		while (fast != null && fast.next != null) {
+
+			slow = slow.next;
+			fast = fast.next.next;
+
+		}
+
+		return slow.data;
+	}
+
+	public int kthFromLast(int k) {
+
+		Node fast = head;
+
+		for (int i = 1; i <= k; i++) {
+			fast = fast.next;
+		}
+
+		Node slow = head;
+
+		while (fast != null) {
+
+			slow = slow.next;
+			fast = fast.next;
+		}
+
+		return slow.data;
+	}
+
+	public void createDummyList() {
+
+		Node n1 = new Node(10);
+		Node n2 = new Node(20);
+		Node n3 = new Node(30);
+		Node n4 = new Node(40);
+		Node n5 = new Node(50);
+		Node n6 = new Node(60);
+		Node n7 = new Node(70);
+		Node n8 = new Node(80);
+		Node n9 = new Node(90);
+		Node n10 = new Node(100);
+		Node n11 = new Node(110);
+		Node n12 = new Node(120);
+		Node n13 = new Node(130);
+
+		n1.next = n2;
+		n2.next = n3;
+		n3.next = n4;
+		n4.next = n5;
+		n5.next = n6;
+		n6.next = n7;
+		n7.next = n8;
+		n8.next = n9;
+		n9.next = n10;
+		n13.next = n12;
+		n12.next = n11;
+		n11.next = n7;
+
+		Node h1 = n1;
+		Node h2 = n13;
+
+		// logic ....
+		System.out.println(intersection(h1, h2));
+
+	}
+
+	private int intersection(Node h1, Node h2) {
+
+		Node fp = h1;
+		Node sp = h2;
+
+		while (fp != sp) {
+
+			if (fp == null)
+				fp = h2;
+			else
+				fp = fp.next;
+
+			if (sp == null)
+				sp = h1;
+			else
+				sp = sp.next;
+		}
+
+		return fp.data;
+	}
 }
