575871e0c04e0166fc493093b3eb18fa2922b2fb
Garima <garimachhikara128@gmail.com>
2021-02-21 11:19:32 +0530

    Binary Tree

--- src/L33_BinaryTree/BTClient.java
+++ src/L33_BinaryTree/BTClient.java
@@ -5,7 +5,13 @@ public class BTClient {
 	public static void main(String[] args) {
 		
 		// 10 true 20 true 40 false false true 50 false false true 30 false true 60 false false
-		BinaryTree bt = new BinaryTree() ;
+		
+		// 10 true 20 true 40 true 60 true 80 false false false false true 50 false true 70 false true 90 false false true 30 false false
+
+		int[] pre = {} ;
+		int[] in = {} ;
+		
+		BinaryTree bt = new BinaryTree(pre, in) ;
 		bt.display();
 		
 		System.out.println(bt.size());
@@ -13,6 +19,17 @@ public class BTClient {
 		System.out.println(bt.find(50));
 		System.out.println(bt.ht());
 		
+		System.out.println(bt.diameter3());
+		
+		System.out.println(bt.isBalanced3());
+		
+//		BinaryTree bt1 = new BinaryTree() ;
+//		bt1.display();
+//		
+//		System.out.println(bt.flipEquivalent(bt1));
+		
+		//bt.preorderI();
+		System.out.println(bt.maxSubtreeSum3());
 
 	}
 
--- src/L33_BinaryTree/BinaryTree.java
+++ src/L33_BinaryTree/BinaryTree.java
@@ -1,6 +1,7 @@
 package L33_BinaryTree;
 
 import java.util.Scanner;
+import java.util.Stack;
 
 public class BinaryTree {
 
@@ -52,6 +53,14 @@ public class BinaryTree {
 			nn.right = construct(nn, false);
 
 		return nn;
+
+	}
+
+	public BinaryTree(int[] pre, int[] in) {
+		root = construct(pre, 0, pre.length - 1, in, 0, in.length - 1);
+	}
+
+	private Node construct(int[] pre, int plo, int phi, int[] in, int ilo, int ihi) {
 
 	}
 
@@ -143,13 +152,379 @@ public class BinaryTree {
 
 	private int ht(Node node) {
 
-		if(node == null)
-			return -1 ;
-		
+		if (node == null)
+			return -1;
+
 		int lh = ht(node.left);
 		int rh = ht(node.right);
 
 		return Math.max(lh, rh) + 1;
+
+	}
+
+	private int max = Integer.MIN_VALUE;
+
+	public int diameter() {
+		diameter(root);
+		return max;
+	}
+
+	private void diameter(Node node) {
+
+		if (node == null)
+			return;
+
+		diameter(node.left);
+		diameter(node.right);
+
+		int presentDiameterRootNode = ht(node.left) + ht(node.right) + 2;
+
+		if (presentDiameterRootNode > max)
+			max = presentDiameterRootNode;
+
+	}
+
+	public int diameter2() {
+		return diameter2(root);
+	}
+
+	private int diameter2(Node node) {
+
+		if (node == null)
+			return 0;
+
+		int ld = diameter2(node.left);
+		int rd = diameter2(node.right);
+
+		int sd = ht(node.left) + ht(node.right) + 2; // self node diameter root node -> max distance ?
+
+		return Math.max(sd, Math.max(ld, rd));
+
+	}
+
+	private class DiaPair {
+		int diameter = 0;
+		int ht = -1;
+	}
+
+	public int diameter3() {
+		return diameter3(root).diameter;
+	}
+
+	private DiaPair diameter3(Node node) {
+
+		if (node == null) {
+			return new DiaPair();
+		}
+
+		DiaPair ldp = diameter3(node.left);
+		DiaPair rdp = diameter3(node.right);
+
+		DiaPair sdp = new DiaPair();
+
+		// sdp diameter
+		int ld = ldp.diameter;
+		int rd = rdp.diameter;
+		int sd = ldp.ht + rdp.ht + 2;
+
+		sdp.diameter = Math.max(sd, Math.max(ld, rd));
+
+		// sdp ht
+		sdp.ht = Math.max(ldp.ht, rdp.ht) + 1;
+
+		return sdp;
+
+	}
+
+	private boolean res = true;
+
+	public boolean isBalanced() {
+		isBalanced(root);
+		return res;
+	}
+
+	private void isBalanced(Node node) {
+
+		if (node == null)
+			return;
+
+		isBalanced(node.left);
+		isBalanced(node.right);
+
+		int bf = ht(node.left) - ht(node.right);
+
+		if (bf <= -2 || bf >= 2)
+			res = false;
+
+	}
+
+	public boolean isBalanced2() {
+		return isBalanced2(root);
+	}
+
+	private boolean isBalanced2(Node node) {
+
+		if (node == null)
+			return true;
+
+		boolean lb = isBalanced2(node.left);
+		boolean rb = isBalanced2(node.right);
+
+		int bf = ht(node.left) - ht(node.right);
+
+		if (lb && rb && (bf == -1 || bf == 0 || bf == 1))
+			return true;
+		else
+			return false;
+	}
+
+	private class BalPair {
+		boolean isBal = true;
+		int ht = -1;
+	}
+
+	public boolean isBalanced3() {
+		return isBalanced3(root).isBal;
+	}
+
+	private BalPair isBalanced3(Node node) {
+
+		if (node == null)
+			return new BalPair();
+
+		BalPair lbp = isBalanced3(node.left);
+		BalPair rbp = isBalanced3(node.right);
+
+		BalPair sbp = new BalPair();
+
+		// sbp isBal
+		boolean lb = lbp.isBal;
+		boolean rb = rbp.isBal;
+		int bf = lbp.ht - rbp.ht;
+
+		if (lb && rb && (bf == -1 || bf == 0 || bf == 1))
+			sbp.isBal = true;
+		else
+			sbp.isBal = false;
+
+		// sbp ht
+		sbp.ht = Math.max(lbp.ht, rbp.ht) + 1;
+
+		return sbp;
+
+	}
+
+	public boolean flipEquivalent(BinaryTree other) {
+
+		return flipEquivalent(this.root, other.root);
+	}
+
+	private boolean flipEquivalent(Node n1, Node n2) {
+
+		if (n1 == null && n2 == null)
+			return true;
+
+		if (n1 == null || n2 == null)
+			return false;
+
+		if (n1.data != n2.data)
+			return false;
+
+		boolean flip = flipEquivalent(n1.left, n2.right) && flipEquivalent(n1.right, n2.left);
+
+		if (flip)
+			return true;
+
+		boolean noflip = flipEquivalent(n1.left, n2.left) && flipEquivalent(n1.right, n2.right);
+
+		return flip || noflip;
+
+	}
+
+	public void display2() {
+		display2(root);
+	}
+
+	private void display2(Node node) {
+
+		if (node == null)
+			return;
+
+		System.out.println("hii " + node.data);
+
+		display2(node.left);
+		System.out.println("coming back from left child and going towards right");
+		display2(node.right);
+
+		System.out.println("byee " + node.data);
+
+	}
+
+	public void preorder() {
+		preorder(root);
+		System.out.println();
+	}
+
+	// NLR : preorder
+	// LNR : inorder
+	// LRN : postorder
+	// NRL : rev postorder
+	// RNL : rev inorder
+	// RLN : rev preorder
+	private void preorder(Node node) {
+
+		if (node == null)
+			return;
+
+		// N
+		System.out.print(node.data + " ");
+
+		// L
+		preorder(node.left);
+
+		// R
+		preorder(node.right);
+
+	}
+
+	private class Pair {
+		Node n;
+		boolean sd;
+		boolean ld;
+		boolean rd;
+	}
+
+	public void preorderI() {
+
+		Stack<Pair> s = new Stack<>();
+
+		Pair sp = new Pair();
+		sp.n = root;
+
+		s.push(sp);
+
+		while (!s.isEmpty()) {
+
+			Pair tp = s.peek();
+
+			if (tp.sd == false) {
+
+				System.out.print(tp.n.data + " ");
+				tp.sd = true;
+
+			} else if (tp.ld == false) {
+
+				Pair lp = new Pair();
+				lp.n = tp.n.left;
+
+				if (lp.n != null)
+					s.push(lp);
+
+				tp.ld = true;
+
+			} else if (tp.rd == false) {
+
+				Pair rp = new Pair();
+				rp.n = tp.n.right;
+
+				if (rp.n != null)
+					s.push(rp);
+
+				tp.rd = true;
+
+			} else {
+				s.pop();
+			}
+
+		}
+
+		System.out.println();
+
+	}
+
+	public int sum() {
+		return sum(root);
+	}
+
+	private int sum(Node node) {
+
+		if (node == null)
+			return 0;
+
+		int ls = sum(node.left);
+		int rs = sum(node.right);
+
+		return ls + rs + node.data;
+	}
+
+	private int maxSum = Integer.MIN_VALUE;
+
+	public int maxSubtreeSum1() {
+		return maxSubtreeSum1(root);
+	}
+
+	private int maxSubtreeSum1(Node node) {
+
+		if (node == null)
+			return 0;
+
+		int ls = maxSubtreeSum1(node.left);
+		int rs = maxSubtreeSum1(node.right);
+
+		int ts = ls + rs + node.data;
+
+		if (ts > maxSum)
+			maxSum = ts;
+
+		return ts;
+
+	}
+
+	public int maxSubtreeSum2() {
+		return maxSubtreeSum2(root);
+	}
+
+	private int maxSubtreeSum2(Node node) {
+
+		if (node == null)
+			return Integer.MIN_VALUE;
+
+		int lMaxSubtreeSum = maxSubtreeSum2(node.left);
+		int rMaxSubtreeSum = maxSubtreeSum2(node.right);
+
+		int selfNodeSum = sum(node.left) + sum(node.right) + node.data;
+
+		return Math.max(selfNodeSum, Math.max(lMaxSubtreeSum, rMaxSubtreeSum));
+
+	}
+
+	// mss : max subtree sum
+	private class MSSPair {
+		int mss = Integer.MIN_VALUE;
+		int entireSum = 0;
+	}
+
+	public int maxSubtreeSum3() {
+		return maxSubtreeSum3(root).mss;
+	}
+
+	private MSSPair maxSubtreeSum3(Node node) {
+
+		if (node == null)
+			return new MSSPair();
+
+		MSSPair lp = maxSubtreeSum3(node.left);
+		MSSPair rp = maxSubtreeSum3(node.right);
+
+		MSSPair sp = new MSSPair();
+
+		// sp maxSubtreeSum
+		sp.mss = Math.max(lp.entireSum + rp.entireSum + node.data, Math.max(lp.mss, rp.mss));
+
+		// sp entiresum
+		sp.entireSum = lp.entireSum + rp.entireSum + node.data;
+
+		return sp;
 
 	}
 
